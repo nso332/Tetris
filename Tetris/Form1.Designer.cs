56899e7c43c94fdae56a32cc9483ec7fccbf558f
Nicholas Otto <43212390+nso332@users.noreply.github.com>
2018-12-06 20:38:04 -0600

    The Start of Tetris
    Started creating the base skeleton for Tetris.

---
+++ Tetris/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Tetris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BlockPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // BlockPanel
+            // 
+            this.BlockPanel.Location = new System.Drawing.Point(17, 21);
+            this.BlockPanel.Name = "BlockPanel";
+            this.BlockPanel.Size = new System.Drawing.Size(350 , 500);
+            this.BlockPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 500);
+            this.Controls.Add(this.BlockPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel BlockPanel;
+    }
+}
+
